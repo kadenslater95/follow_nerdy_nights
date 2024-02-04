7b20e68e5d5c338a4c50c8c8683a8451d21b2608
kadenslater95 <kadenslater95@gmail.com>
2024-02-04 14:13:55 -0600

    More Annotations

--- week3/background/background.s
+++ week3/background/background.s
@@ -3,6 +3,8 @@
 	; See https://www.masswerk.at/6502/6502_instruction_set.html for list of Instructions 
 	;
 	; $ inidicates we are writing a hex number
+	; % indiciates we are writing a binary number
+	; No prefix indicates we are writing a decimal number (never write decimal because it is disabled)
 	;
 	; When working with instructions:
 		; # indicates we are giving an immediate value, so #$00 is 0,
@@ -10,6 +12,47 @@
 	;
 	; When working with Directives (such as .byte) the # is not used
 		; so it is still an immediate value (though that value can be referencing an address)
+	;
+	; Acronyms and Some Definitions:
+		; PPU is Picture Processing Unit, a processor separate from CPU that passes data to CRT or
+		; whatever display you are using.
+		; APU is Audio Processing Unit, another separate processor on the Motherboard, and it handles sound.
+		; Note: You write code that runs in the CPU, but through certain Memory Addresses you can
+		; control the PPU and APU and through other address ranges you can pass data to them.
+		;
+		; NMI is Non Maskable Interrupt https://en.wikipedia.org/wiki/Non-maskable_interrupt
+		;
+		; IRQ is Interrupt Request https://en.wikipedia.org/wiki/Interrupt_request
+		;
+		; CRT is Cathode Ray Tube, the TV that renders things in Scanlines (to the program it pretends
+		; that everything is done in scanlines regardless of display, I just say CRT for simplicity)
+		;
+		; BG is background
+		;
+		; FCEUX and Nestopia are programs that run your iNES (.nes extended) file after you Assemble 
+		; and Link it.
+	;
+	; Unexpected / Sneaky things:
+		; You have to set the PPU Read/Write Address by consecutively writing to the byte at
+		; address $2006 twice.
+		; It's unexpected because you have to specify a 2 byte address by writing to 1 byte and then
+		; writing to it again, rather than writing to 2 separate bytes.
+		; The first write specifies the High byte of the address and the second write specifies
+		; the Low byte of the address.
+			; FIGURED OUT WHY: As I got further along I realized that PPU Memory is separate from the RAM
+			; and that the PPU accesses RAM that the CPU has access to in order to get instructions, and
+			; data. So when I want to access PPU Memory I have to do so via a combination of writes 
+			; to control memory addresses and writes to data memory addresses.
+			; My flawed expectation was that I would write a whole block of bytes and then tell the PPU
+			; what the start and end of those bytes was, but that would take up space on the RAM to fill
+			; up all those bytes I want to write to the PPU, and RAM is already pretty limited, and if I
+			; were to read data from the PPU it would probably take up all the CPU RAM.
+			; Instead you write 1 byte at a time to a single byte, over and over.
+			; Now I can Write to the PPU Memory without taking up space in the CPU RAM.
+			; Also, the reason for writing to $2006 in CPU RAM is to set what address in PPU Memory
+			; you want to do the reading and writing from, so the $2006 byte and the $2007 byte are the
+			; only 2 bytes needed to Reader/Write all the data you could ever want From/To the PPU Memory.
+	;
 
 
 .segment "HEADER"
@@ -53,38 +96,53 @@ vblankwait1:
 									; of this Byte so we can loop to wait by branching with the next instruction.
 	bpl	vblankwait1	; Branches if Negative status flag is 0, so loop until PPU changes
 									; that PPU VBlank Ready Status bit 7 via the Byte $2002
-	; I think the reason for doing this initial vblankwait is because before we start clearing the
-	; screen in the next block we want to time it so that we don't clear the screen while the PPU is
-	; in the middle of pulling those memory addresses.
-	; Ideally, the CRT will have already rendered $0000 to $0100 by the time we overwrite the memory
-	; at those bytes with a blank background so that those bytes were already rendered from before.
-	; This way it is less likely that it will look like it is broken because if we didn't time this
-	; background clearing then half of whatever image was getting rendered will become blanked out in
-	; the bottom scanlines.
 
+; Note that the CPU has dedicated bytes $0000 to $0800 to Internal RAM use, so this is what we
+; are clearing out in this next block
 clear_memory:
-	lda	#$00
-	sta	$0000, x
+	lda	#$00					; Overwriting to 0 will be used to clear the Bytes
+
+										; X at this point is still 0 on the first Branching Iteration since we
+										; haven't modified it since the Reset Block
+	sta	$0000, x		
 	sta	$0100, x
 	sta	$0200, x
-	sta	$0300, x
-	sta	$0400, x
-	sta	$0500, x
-	sta	$0600, x
-	sta	$0700, x
-	inx
-	bne	clear_memory
+	sta	$0300, x			; Each of these is storing the A register which is currently 0 in a Byte at
+	sta	$0400, x			; at the given address which is offset by X.
+	sta	$0500, x			; Note that X will loop from 0 to 255 then the branch will end back at 0.
+	sta	$0600, x			; Only need to offset by every 256 bytes or $0100 since we offset the X
+	sta	$0700, x			; to write to all the bytes in between.
+
+										; The first Increment of X makes it 1 before checking Zero flag with BNE
+	inx								; Keep incrementing the X register, eventually it will wrap back around to 0 
+										; after the 256th increment
+	bne	clear_memory	; When INX wraps X back to 0 on the 256th iteration the Zero flag will be set
+										; and we will no longer loop
 
 	;; second wait for vblank, PPU is ready after this
+	; Later on the vblank wait block should be moved to a stored procedure because that would
+	; be useful since there are situations that we want to do this
 vblankwait2:
 	bit	$2002
 	bpl	vblankwait2
 
-clear_palette:	
-	;; Need clear both palettes to $00. Needed for Nestopia. Not
-	;; needed for FCEU* as they're already $00 on powerup.
+;; Need clear both palettes to $00. Needed for Nestopia. Not
+;; needed for FCEU* as they're already $00 on powerup.
+clear_palette:
+	; The CPU Cannot directly access the Color Palette memory, this is accessed by the PPU.
+	; So I must through memory tell the PPU that I am going to write to the Color
+	; Palette addresses, then tell it what data to write. 
+	
+	; First reset the PPU Read/Write Address
 	lda	$2002		; Read PPU status to reset PPU address
-	lda	#$3f		; Set PPU address to BG palette RAM ($3F00)
+	
+	; Set PPU address to BG (background) palette RAM Address ($3F00)
+	; Note that you write the 2 Byte Address for the PPU to Read/Write from by writing
+	; the High Byte to $2006 and then immediately following that with a write of the Low
+	; Byte to $2006.
+	; So 1 byte gets used to set that 2 Byte address by writing to it twice, rather than using
+	; 2 separate bytes to specify that 2 Byte address.
+	lda	#$3f		
 	sta	$2006
 	lda	#$00
 	sta 	$2006
@@ -92,12 +150,13 @@ clear_palette:
 	ldx	#$20		; Loop $20 times (up to $3F20)
 	lda	#$00		; Set each entry to $00
 @loop:
-	sta	$2007
+	sta	$2007		; $2007 is that 1 byte that I Write to over and over again to pass data to PPU
+							; memory 1 byte at a time
 	dex
 	bne	@loop
 
-	lda	#%10000000	; intensify blues
-	sta	$2001
+	lda	#%01000000	; PPUMask, the first 3 bits (RGB) set color emphasis for what Blank (no color) is 
+	sta	$2001				; $2001 is the PPUMask CPU RAM Address
 
 forever:
 	jmp	forever
